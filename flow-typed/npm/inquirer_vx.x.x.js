34299329c007c60e8409edc20eca9692e083e0ee
dantesolis <dantevs@icloud.com>
2019-07-03 17:24:20 +0200

    Started adding type definitions for inquirer lib 😱

--- flow-typed/npm/inquirer_vx.x.x.js
+++ flow-typed/npm/inquirer_vx.x.x.js
@@ -14,7 +14,33 @@
  */
 
 declare module 'inquirer' {
-  declare module.exports: any;
+  // declare module.exports: any // [ASK]: came with the stub maybe need to uncommented later
+
+  declare export type Inquirer = Object
+  declare export type Prompt = Object
+  declare export type Questions = Array<Question>
+  declare export type Answers = Array
+
+  declare export type Question = {|
+    type?: string,
+    name?: string,
+    message?: string | Function,
+    default?: string | number | boolean | Array<any> | Function,
+    choices?: Array<any> | Function,
+    validate?: Function,
+    filter?: Function,
+    transformer?: Function,
+    when?: Function | boolean,
+    pageSize?: number,
+    prefix?: string,
+    suffix?: string,
+  |}
+
+  declare module.exports: {
+    prompt(questions: Questions): Promise<>,
+    registerPrompt(name: string, prompt: Prompt): void,
+    createPromptModule(): typeof prompt, // [ASK] this returns the prompt method
+  }
 }
 
 /**
@@ -23,157 +49,157 @@ declare module 'inquirer' {
  * needed.
  */
 declare module 'inquirer/lib/inquirer' {
-  declare module.exports: any;
+  declare module.exports: any
 }
 
 declare module 'inquirer/lib/objects/choice' {
-  declare module.exports: any;
+  declare module.exports: any
 }
 
 declare module 'inquirer/lib/objects/choices' {
-  declare module.exports: any;
+  declare module.exports: any
 }
 
 declare module 'inquirer/lib/objects/separator' {
-  declare module.exports: any;
+  declare module.exports: any
 }
 
 declare module 'inquirer/lib/prompts/base' {
-  declare module.exports: any;
+  declare module.exports: any
 }
 
 declare module 'inquirer/lib/prompts/checkbox' {
-  declare module.exports: any;
+  declare module.exports: any
 }
 
 declare module 'inquirer/lib/prompts/confirm' {
-  declare module.exports: any;
+  declare module.exports: any
 }
 
 declare module 'inquirer/lib/prompts/editor' {
-  declare module.exports: any;
+  declare module.exports: any
 }
 
 declare module 'inquirer/lib/prompts/expand' {
-  declare module.exports: any;
+  declare module.exports: any
 }
 
 declare module 'inquirer/lib/prompts/input' {
-  declare module.exports: any;
+  declare module.exports: any
 }
 
 declare module 'inquirer/lib/prompts/list' {
-  declare module.exports: any;
+  declare module.exports: any
 }
 
 declare module 'inquirer/lib/prompts/number' {
-  declare module.exports: any;
+  declare module.exports: any
 }
 
 declare module 'inquirer/lib/prompts/password' {
-  declare module.exports: any;
+  declare module.exports: any
 }
 
 declare module 'inquirer/lib/prompts/rawlist' {
-  declare module.exports: any;
+  declare module.exports: any
 }
 
 declare module 'inquirer/lib/ui/baseUI' {
-  declare module.exports: any;
+  declare module.exports: any
 }
 
 declare module 'inquirer/lib/ui/bottom-bar' {
-  declare module.exports: any;
+  declare module.exports: any
 }
 
 declare module 'inquirer/lib/ui/prompt' {
-  declare module.exports: any;
+  declare module.exports: any
 }
 
 declare module 'inquirer/lib/utils/events' {
-  declare module.exports: any;
+  declare module.exports: any
 }
 
 declare module 'inquirer/lib/utils/paginator' {
-  declare module.exports: any;
+  declare module.exports: any
 }
 
 declare module 'inquirer/lib/utils/readline' {
-  declare module.exports: any;
+  declare module.exports: any
 }
 
 declare module 'inquirer/lib/utils/screen-manager' {
-  declare module.exports: any;
+  declare module.exports: any
 }
 
 declare module 'inquirer/lib/utils/utils' {
-  declare module.exports: any;
+  declare module.exports: any
 }
 
 // Filename aliases
 declare module 'inquirer/lib/inquirer.js' {
-  declare module.exports: $Exports<'inquirer/lib/inquirer'>;
+  declare module.exports: $Exports<'inquirer/lib/inquirer'>
 }
 declare module 'inquirer/lib/objects/choice.js' {
-  declare module.exports: $Exports<'inquirer/lib/objects/choice'>;
+  declare module.exports: $Exports<'inquirer/lib/objects/choice'>
 }
 declare module 'inquirer/lib/objects/choices.js' {
-  declare module.exports: $Exports<'inquirer/lib/objects/choices'>;
+  declare module.exports: $Exports<'inquirer/lib/objects/choices'>
 }
 declare module 'inquirer/lib/objects/separator.js' {
-  declare module.exports: $Exports<'inquirer/lib/objects/separator'>;
+  declare module.exports: $Exports<'inquirer/lib/objects/separator'>
 }
 declare module 'inquirer/lib/prompts/base.js' {
-  declare module.exports: $Exports<'inquirer/lib/prompts/base'>;
+  declare module.exports: $Exports<'inquirer/lib/prompts/base'>
 }
 declare module 'inquirer/lib/prompts/checkbox.js' {
-  declare module.exports: $Exports<'inquirer/lib/prompts/checkbox'>;
+  declare module.exports: $Exports<'inquirer/lib/prompts/checkbox'>
 }
 declare module 'inquirer/lib/prompts/confirm.js' {
-  declare module.exports: $Exports<'inquirer/lib/prompts/confirm'>;
+  declare module.exports: $Exports<'inquirer/lib/prompts/confirm'>
 }
 declare module 'inquirer/lib/prompts/editor.js' {
-  declare module.exports: $Exports<'inquirer/lib/prompts/editor'>;
+  declare module.exports: $Exports<'inquirer/lib/prompts/editor'>
 }
 declare module 'inquirer/lib/prompts/expand.js' {
-  declare module.exports: $Exports<'inquirer/lib/prompts/expand'>;
+  declare module.exports: $Exports<'inquirer/lib/prompts/expand'>
 }
 declare module 'inquirer/lib/prompts/input.js' {
-  declare module.exports: $Exports<'inquirer/lib/prompts/input'>;
+  declare module.exports: $Exports<'inquirer/lib/prompts/input'>
 }
 declare module 'inquirer/lib/prompts/list.js' {
-  declare module.exports: $Exports<'inquirer/lib/prompts/list'>;
+  declare module.exports: $Exports<'inquirer/lib/prompts/list'>
 }
 declare module 'inquirer/lib/prompts/number.js' {
-  declare module.exports: $Exports<'inquirer/lib/prompts/number'>;
+  declare module.exports: $Exports<'inquirer/lib/prompts/number'>
 }
 declare module 'inquirer/lib/prompts/password.js' {
-  declare module.exports: $Exports<'inquirer/lib/prompts/password'>;
+  declare module.exports: $Exports<'inquirer/lib/prompts/password'>
 }
 declare module 'inquirer/lib/prompts/rawlist.js' {
-  declare module.exports: $Exports<'inquirer/lib/prompts/rawlist'>;
+  declare module.exports: $Exports<'inquirer/lib/prompts/rawlist'>
 }
 declare module 'inquirer/lib/ui/baseUI.js' {
-  declare module.exports: $Exports<'inquirer/lib/ui/baseUI'>;
+  declare module.exports: $Exports<'inquirer/lib/ui/baseUI'>
 }
 declare module 'inquirer/lib/ui/bottom-bar.js' {
-  declare module.exports: $Exports<'inquirer/lib/ui/bottom-bar'>;
+  declare module.exports: $Exports<'inquirer/lib/ui/bottom-bar'>
 }
 declare module 'inquirer/lib/ui/prompt.js' {
-  declare module.exports: $Exports<'inquirer/lib/ui/prompt'>;
+  declare module.exports: $Exports<'inquirer/lib/ui/prompt'>
 }
 declare module 'inquirer/lib/utils/events.js' {
-  declare module.exports: $Exports<'inquirer/lib/utils/events'>;
+  declare module.exports: $Exports<'inquirer/lib/utils/events'>
 }
 declare module 'inquirer/lib/utils/paginator.js' {
-  declare module.exports: $Exports<'inquirer/lib/utils/paginator'>;
+  declare module.exports: $Exports<'inquirer/lib/utils/paginator'>
 }
 declare module 'inquirer/lib/utils/readline.js' {
-  declare module.exports: $Exports<'inquirer/lib/utils/readline'>;
+  declare module.exports: $Exports<'inquirer/lib/utils/readline'>
 }
 declare module 'inquirer/lib/utils/screen-manager.js' {
-  declare module.exports: $Exports<'inquirer/lib/utils/screen-manager'>;
+  declare module.exports: $Exports<'inquirer/lib/utils/screen-manager'>
 }
 declare module 'inquirer/lib/utils/utils.js' {
-  declare module.exports: $Exports<'inquirer/lib/utils/utils'>;
+  declare module.exports: $Exports<'inquirer/lib/utils/utils'>
 }
